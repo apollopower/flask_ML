82569ea800ee68db846ff7b126c1c7ec91f04f68
Jonas <erthaljonas@gmail.com>
2019-01-30 10:41:20 -0500

    Working Linear Regression

--- learner/data_prep.py
+++ learner/data_prep.py
@@ -1,16 +1,24 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
-def prep_data(data_source, csv=True):
-    if csv:
-        dataframe = pd.read_csv(data_source)
-        return dataframe
-
-def supervised_data(dataframe, features, target):
-    data_map = {}
-
-    data_map[x_train] = dataframe[features]
-    data_map[y_train] = dataframe[target]
-
+def create_supervised_dataset(data_path, features, target, csv=True):
+    if not csv:
+        return "Error, data not CSV" # Hack solution for now
+    else:
+        df = pd.read_csv(data_path)
+    data_map = {
+            'x_train': df[features],
+            'y_train': df[target]
+            }
     return data_map
+
+def linear_regression(data_map):
+   linear_reg = LinearRegression()
+   linear_reg.fit(data_map['x_train'], data_map['y_train'])
+   y_predict = linear_reg.predict(data_map['x_train'])
+   print("Trained!")
+   print("-" * 80)
+   print("Mean Absolute Error {}".format(metrics.mean_absolute_error(data_map['y_train'], y_predict)))
 
